403521624f802699ad16096192d24857da23ac10
Morel Sébastien <morel.seb@gmail.com>
2017-10-29 18:04:58 -0700

    Fix run varnish

--- start.bash
+++ start.bash
@@ -2,7 +2,5 @@
 set -e
 
 exec bash -c \
-  "exec varnishd -F -u varnish \
-  -f $VCL_CONFIG \
-  -s malloc,$CACHE_SIZE \
+  "exec varnishd -F -f $VCL_CONFIG -s malloc,$CACHE_SIZE \
   $VARNISHD_PARAMS"
